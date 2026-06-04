49da6a0a97a54b72e5b711e28fa20a27d8ed1065
Boollay32 <124304930+Boollay32@users.noreply.github.com>
2026-06-04 14:35:49 +0100

    Add files via upload

---
+++ Views/Shared/_FilterBox.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelpDeskNet8.Views.Shared
+{
+    public class FilterBoxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
